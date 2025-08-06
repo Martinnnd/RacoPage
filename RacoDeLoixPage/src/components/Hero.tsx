7026243920c85b3befcdcce24dbc6b3237de4d0c
Martin de Oro <deoromartinignacio@gmail.com>
2025-08-06 19:14:43 -0300

    se actualiza la imagen hero

--- RacoDeLoixPage/src/components/Hero.tsx
+++ RacoDeLoixPage/src/components/Hero.tsx
@@ -5,7 +5,7 @@ const Hero = () => {
     <section
       className="h-screen bg-cover bg-center flex flex-col items-center justify-center text-white text-center px-4"
       style={{
-        backgroundImage: "url('/HeroRaco.jpg')", 
+        backgroundImage: "url('/heroRacoV2.jpg')", 
       }}
     >
     </section>
